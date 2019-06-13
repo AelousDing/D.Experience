13900fcf956961fbabcd3324a7fca7bf2e6c509f
fengjuanchensha <1130217572@qq.com>
2019-06-13 16:45:34 +0800

    添加一种全球化的实现方式
    添加一种全球化的实现方式

---
+++ MultiLanguage/Language.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiLanguage
+{
+    /// <summary>
+    /// 调用方式
+    /// XAML：
+    /// xmlns:lan="clr-namespace:MultiLanguage.Properties;assembly=MultiLanguage"
+    /// <Button Grid.Row="5" Content="{x:Static lan:Resources.UserName}" Command="{Binding LogOnCommand}" FontSize="16" Height="40" VerticalAlignment="Top" IsDefault="True"/>
+    /// .CS：MultiLanguage.Properties.Resources.UserName
+    /// </summary>
+    public class Language
+    {
+        public static void SetLanguage(string region)
+        {
+            CultureInfo ci = new CultureInfo(region);
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+    }
+}
