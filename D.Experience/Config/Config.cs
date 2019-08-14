ea6d7aa634b528457bd5ec9cc54d1e59e9aa34be
fengjuanchensha <1130217572@qq.com>
2019-08-14 08:48:15 +0800

    配置文件的一种实现方式
    配置文件的一种实现方式

---
+++ D.Experience/Config/Config.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Core
+{
+    public class Config
+    {
+        /// <summary>
+        /// ctc数据地址
+        /// </summary>
+        public string CtcPlanAddress { get; set; }
+        /// <summary>
+        /// 主服务端口
+        /// </summary>
+        public int Port { get; set; }
+        /// <summary>
+        /// ctc数据端口
+        /// </summary>
+        public int CtcPlanPort { get; set; }
+        /// <summary>
+        /// 数据库连接字符串
+        /// </summary>
+        public string ConnectionString { get; set; }
+        /// <summary>
+        /// 数据中心编号
+        /// </summary>
+        public int DataCenterId { get; set; }
+        /// <summary>
+        /// 服务器编号
+        /// </summary>
+        public int ServerId { get; set; }
+    }
+}
---
+++ D.Experience/Config/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Core
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            AppEnvironment.Config = LoadConfig();
+        }
+
+        private Config LoadConfig()
+        {
+            Config config = new Config();
+            PropertyInfo[] properties = config.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                string propertyValue = ConfigurationManager.AppSettings[property.Name];
+                if (!property.PropertyType.IsGenericType)
+                {
+                    property.SetValue(config, string.IsNullOrEmpty(propertyValue) ? null : Convert.ChangeType(propertyValue, property.PropertyType), null);
+                }
+                else
+                {
+                    Type type = property.PropertyType.GetGenericTypeDefinition();
+                    if (type == typeof(Nullable<>))
+                    {
+                        property.SetValue(config, string.IsNullOrEmpty(propertyValue) ? null : Convert.ChangeType(propertyValue, Nullable.GetUnderlyingType(property.PropertyType)), null);
+                    }
+                }
+            }
+            return config;
+        }
+    }
+}
